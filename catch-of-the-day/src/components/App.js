93101248c032f13d5c2cf6fef800a8a02c00f81d
Colin Lord <colin@kt4atl.com>
2020-01-02 07:59:17 -0600

    21 - Remove from Order Button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -41,6 +41,15 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  deleteFish = (key) => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. Update the state
+    fishes[key] = null;
+    // 3. Update state
+    this.setState({ fishes });
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -53,6 +62,15 @@ class App extends React.Component {
     // 3. Call setState to update our state object
     this.setState({ order });
   };
+
+  removeFromOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Remove from order
+    delete order[key];
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  }
 
   render() {
     return (
@@ -70,12 +88,13 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
+          deleteFish={this.deleteFish}
         />
       </div>
     );
