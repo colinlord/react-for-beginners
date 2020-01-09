fd3c144121152e59eb9e2946e1a27f02372aa957
Colin Lord <colin@kt4atl.com>
2020-01-09 14:18:14 -0600

    Add the build

---
+++ catch-of-the-day/build/precache-manifest.4462eb307644eb974dc6c3f0ea638645.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a5ef45ddd356720ddb",
+    "url": "/static/css/main.e0e3bc45.chunk.css"
+  },
+  {
+    "revision": "26a5ef45ddd356720ddb",
+    "url": "/static/js/main.5f30286d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "960c2d579615d73dd195",
+    "url": "/static/js/2.412b8f8f.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "df09543ec79277b4fa2483343da1f93f",
+    "url": "/index.html"
+  }
+];
